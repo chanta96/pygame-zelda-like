e416b51000e3bf96f7174996374361964fbfda4a
Enzo Gonzalo Gramajo <112021063+chanta96@users.noreply.github.com>
2023-05-28 15:09:54 -0300

    added level, tile, and the graphics files

---
+++ level.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Level:
+    def __init__(self):
+        # get display surface
+        self.display_surface = pygame.display.get_surface()
+        # sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+    def run(self):
+        # update an draw the game
+        pass
